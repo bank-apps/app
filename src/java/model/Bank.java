6de06b29854cf1e18e12249ee3c9f7afffd01a75
SamuelRodri <samuelrh2001@hotmail.com>
2022-03-22 12:17:01 +0000

    Bank Class Added

---
+++ src/java/model/Bank.java
@@ -0,0 +1,15 @@
+package model;
+
+
+public class Bank {
+    
+    public void register(UserData data){
+        
+    }
+    
+    public void login(String dni, String passwd){
+        
+    }
+    
+    
+}
