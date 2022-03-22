428cea44c0973f3ac5562475adb9ca986deb59da
Aashish Hathiramani <hathiramaniaashish@gmail.com>
2022-03-22 18:49:19 +0000

    Trying SQLite Connection

--- src/java/model/Bank.java
+++ src/java/model/Bank.java
@@ -1,13 +1,20 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Bank {
     
-    public void register(UserData data){
+    public void register(UserData userData) throws SQLException {
+        /*String URL = "jdbc:sqlite:BankApp.db";
+        Connection conn = DriverManager.getConnection(URL);
+        System.out.println("Connection Established!");*/
         
     }
     
-    public void login(String dni, String passwd){
+    public void login(String DNI, String password){
         
     }
     
---
+++ src/java/model/Main.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.sql.SQLException;
+
+public class Main {
+    
+    public static void main(String[] args) throws SQLException {
+
+    }
+}
