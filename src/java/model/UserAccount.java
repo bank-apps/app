4878e3f7b0034c468848080e2eb8cf518316c253
Aashish Hathiramani <hathiramaniaashish@gmail.com>
2022-03-22 12:17:12 +0000

    BankAccount class - Version 1

--- src/java/model/UserAccount.java
+++ src/java/model/UserAccount.java
@@ -1,8 +1,11 @@
 package model;
 
+import java.util.ArrayList;
+
 public class UserAccount {
     
     private UserData userData;
+    private ArrayList<BankAccount> bankAccounts;
 
     public UserAccount(UserData userData) {
         this.userData = userData;
@@ -12,8 +15,16 @@ public class UserAccount {
         return userData;
     }
 
-    public void setUserData(UserData userData) {
+    public void setData(UserData userData) {
         this.userData = userData;
+    }
+    
+    public void addBankAccount(BankAccount bankAccount) {
+        bankAccounts.add(bankAccount);
+    }
+    
+    public void removeBankAccount(BankAccount bankAccount) {
+        bankAccounts.remove(bankAccount);
     }
     
 }
