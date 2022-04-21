c94e6845e54684041b31dbced9d37ac2e22b09ac
G10Mini <jitenparwani1c17@gmail.com>
2022-04-21 13:13:00 +0100

    Try

--- src/java/model/main.java
+++ src/java/model/main.java
@@ -7,8 +7,8 @@ public class main {
     public static void main(String[] args) throws Exception {
         Bank bank = new Bank();
         
-        UserData data = new UserData("'311113411E'", "12333", "'Pepe4'", "'4Martínez Castillo'", "'pepe5mc@hotma3il.com'", "'4i casa'", "12344567489131");
-        
+        UserData data = new UserData("'111113411E'", "12333", "'Pepe4'", "'4Martínez Castillo'", "'pepe4mc@hotma3il.com'", "'4i casa'", "12344567489130");
+                
         System.out.println("Testing Register:");
         System.out.println(bank.register(data));
         
