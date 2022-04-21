ea97eaaf1b135853827eaf136787d5609ac6f9ce
G10Mini <jitenparwani1c17@gmail.com>
2022-04-21 13:06:27 +0100

    Added Testing for Login Function

--- src/java/model/main.java
+++ src/java/model/main.java
@@ -7,8 +7,12 @@ public class main {
     public static void main(String[] args) throws Exception {
         Bank bank = new Bank();
         
-        UserData data = new UserData("'211113411E'", "12333", "'Pepe4'", "'4Martínez Castillo'", "'pepe4mc@hotma3il.com'", "'4i casa'", "12344567489130");
-                
+        UserData data = new UserData("'311113411E'", "12333", "'Pepe4'", "'4Martínez Castillo'", "'pepe5mc@hotma3il.com'", "'4i casa'", "12344567489131");
+        
+        System.out.println("Testing Register:");
         System.out.println(bank.register(data));
+        
+        System.out.println("Testing Login:");
+        System.out.println(bank.login("'311113411E'", "12333"));
     }
 }
